0afa4d90e6af61811800bb76c1587149e8b3d8ef
Tristan <tristan.harris520@gmail.com>
2025-11-14 10:52:51 +1100

    update smart split resizing keybindings

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -1,10 +1,10 @@
 return {
     "mrjones2014/smart-splits.nvim",
     config = function()
-        vim.keymap.set("n", "<C-S-h>", require("smart-splits").resize_left)
-        vim.keymap.set("n", "<C-S-j>", require("smart-splits").resize_down)
-        vim.keymap.set("n", "<C-S-k>", require("smart-splits").resize_up)
-        vim.keymap.set("n", "<C-S-l>", require("smart-splits").resize_right)
+        vim.keymap.set("n", "<C-A-h>", require("smart-splits").resize_left)
+        vim.keymap.set("n", "<C-A-j>", require("smart-splits").resize_down)
+        vim.keymap.set("n", "<C-A-k>", require("smart-splits").resize_up)
+        vim.keymap.set("n", "<C-A-l>", require("smart-splits").resize_right)
 
         -- moving between splits
         vim.keymap.set("n", "<A-h>", require("smart-splits").move_cursor_left)
