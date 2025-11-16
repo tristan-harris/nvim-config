748189bda0aa7fa9a5423c1807ab0abde0f7b2fd
Tristan <tristan.harris520@gmail.com>
2025-11-16 17:18:51 +1100

    better switch source/header function that doesn't rely on clangd

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -1,1 +1,19 @@
-vim.keymap.set("n", "<leader>cs", "<cmd>LspClangdSwitchSourceHeader<cr>", { desc = "[s]witch between source/header" })
+local function switch_source_header()
+    local current_file = vim.api.nvim_buf_get_name(0)
+    local file_basename = vim.fn.fnamemodify(current_file, ":t:r")
+    local file_extension = vim.fn.fnamemodify(current_file, ":e")
+    local corresponding_file
+
+    if file_extension == "h" then
+        corresponding_file = file_basename .. ".c"
+    else
+        corresponding_file = file_basename .. ".h"
+    end
+
+    local old_path = vim.opt.path:get()
+    vim.opt.path = { "**" }
+    vim.cmd("find " .. corresponding_file)
+    vim.opt.path = old_path
+end
+
+vim.keymap.set("n", "<leader>cs", switch_source_header, { desc = "[s]witch between source/header" })
