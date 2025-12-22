e8c7ccaa081e2c399be4a62595b81cda05257993
Tristan <tristan.harris520@gmail.com>
2025-12-23 09:50:16 +1100

    output neater error message if jump to corresponding source/header file in C project files

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -2,8 +2,8 @@ local function switch_source_header()
     local current_file = vim.api.nvim_buf_get_name(0)
     local file_basename = vim.fn.fnamemodify(current_file, ":t:r")
     local file_extension = vim.fn.fnamemodify(current_file, ":e")
-    local corresponding_file
 
+    local corresponding_file
     if file_extension == "h" then
         corresponding_file = file_basename .. ".c"
     else
@@ -12,8 +12,18 @@ local function switch_source_header()
 
     local old_path = vim.opt.path:get()
     vim.opt.path = { "**" }
-    vim.cmd("find " .. corresponding_file)
+
+    local ok, _ = pcall(vim.cmd, "find " .. corresponding_file)
+
     vim.opt.path = old_path
+
+    if not ok then
+        vim.notify(
+            "No corresponding file found: " .. corresponding_file,
+            vim.log.levels.WARN,
+            { title = "switch_source_header" }
+        )
+    end
 end
 
 vim.keymap.set("n", "<leader>cs", switch_source_header, { desc = "[s]witch between source/header" })
