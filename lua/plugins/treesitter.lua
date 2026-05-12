81cb4282001f6e98a880fe4592a83bdd2f8ec86e
Tristan <budgieknight@gmail.com>
2026-05-12 19:40:39 +1000

    disable treesitter indentation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -43,14 +43,14 @@ return {
             -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
             -- vim.wo.foldmethod = 'expr'
 
-            -- check if treesitter indentation is available for this language, and if so enable it
-            -- in case there is no indent query, the indentexpr will fallback to the vim's built in one
-            local has_indent_query = vim.treesitter.query.get(language, "indents") ~= nil
-
-            -- enables treesitter based indentation
-            if has_indent_query then
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end
+            -- -- check if treesitter indentation is available for this language, and if so enable it
+            -- -- in case there is no indent query, the indentexpr will fallback to the vim's built in one
+            -- local has_indent_query = vim.treesitter.query.get(language, "indents") ~= nil
+            --
+            -- -- enables treesitter based indentation
+            -- if has_indent_query then
+            --     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            -- end
         end
 
         local available_parsers = require("nvim-treesitter").get_available()
