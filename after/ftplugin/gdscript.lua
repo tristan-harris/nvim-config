0366d6387ea356da08d0cd9f790d68e210eca5dd
Tristan <budgieknight@gmail.com>
2026-05-12 19:41:43 +1000

    use TAB characters in GDScript
    No longer swim against the tide

--- after/ftplugin/gdscript.lua
+++ after/ftplugin/gdscript.lua
@@ -1,1 +1,2 @@
-vim.opt.expandtab = true -- override built-in ftplugin for gdscript
+-- vim.opt.expandtab = true -- override built-in ftplugin for gdscript
+vim.opt.list = false -- hide tab characters
